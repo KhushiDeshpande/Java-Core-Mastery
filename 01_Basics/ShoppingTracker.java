fffe6bf1b3603262fc2535a2051051431ec6e889
KhushiDeshpande <deshpandekhushi24@gmail.com>
2025-12-26 12:13:55 +0530

    feat: finalize shopping tracker with budget validation
    Added bank balance logic to handle different transaction scenarios

--- 01_Basics/ShoppingTracker.java
+++ 01_Basics/ShoppingTracker.java
@@ -15,17 +15,23 @@ public class ShoppingTracker {
         System.out.print("How many are you getting? "); // taking input
         int quantity = scanner.nextInt();
 
+        System.out.println("Enter bank balance: "); // asking bankbalance from user
+        double bankBalance = scanner.nextDouble(); 
+
         double totalCost = price * quantity; //calculating cost
 
         System.out.println("\n Total Bill ");
         System.out.println("Item: " + item);
         System.out.println("Total: $" + totalCost);
 
-        if (totalCost > 500) {
-            System.out.println("Status: That's a bit expensive, maybe wait for a sale?");
-        } 
-        else {
-            System.out.println("Status: Price looks good. Go for it!");
+        if (bankBalance<totalCost){
+            System.out.println("TRANSACTION REJECTED: You're too broke for this " + item);
+        }
+        else if (bankBalance==totalCost){
+            System.out.println("Living on the edge, aren't we? Transaction successful.");
+        }
+        else{
+            System.out.println("Purchase successful. Remaining balance: " + (bankBalance - totalCost));
         }
 
         scanner.close();
